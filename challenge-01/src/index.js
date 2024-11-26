c4f24a6b60b938806f7f9301b1926283348d4841
Freskies <43386854+Freskies@users.noreply.github.com>
2024-11-26 10:04:13 +0100

    challenge1

---
+++ challenge-01/src/index.js
@@ -0,0 +1,54 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const valerio = {
+	image: "https://www.fibisromagna.it/foto/6098Foto_6992.jpg",
+	longName: "Giacchini Valerio",
+	description:
+		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto officia magni saepe! Dicta, exercitationem sit nobis praesentium quis vitae neque illo rem quaerat autem eligendi molestias omnis ab doloribus incidunt!",
+	skills: [
+		{ skillName: "HTML+CSS", color: "#00f" },
+		{ skillName: "Javascript", color: "#ff0" },
+		{ skillName: "Web Design", color: "#0f0" },
+		{ skillName: "Git and Github", color: "#f00" },
+		{ skillName: "React", color: "#0ff" },
+		{ skillName: "Python", color: "#f0f" },
+	],
+};
+
+function App() {
+	return <Card person={valerio} />;
+}
+
+function Card({ person: { image: url, longName, description, skills } }) {
+	return (
+		<article className="card">
+			<img src={url} alt={longName} />
+			<h3>{longName}</h3>
+			<p>{description}</p>
+			<Skills skills={skills} />
+		</article>
+	);
+}
+
+function Skills({ skills }) {
+	return (
+		<div className="skills">
+			{skills.map(skill => (
+				<Skill skill={skill} />
+			))}
+		</div>
+	);
+}
+
+function Skill({ skill: { skillName, color } }) {
+	return <span style={{ backgroundColor: color }}>{skillName}</span>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+);
