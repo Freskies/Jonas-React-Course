4aaa74e64665ad022144d0263074ef111bc82b74
Freskies <43386854+Freskies@users.noreply.github.com>
2025-01-22 13:22:46 +0100

    challenge2

--- challenge-02/src/App.jsx
+++ challenge-02/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
+	const [step, setStep] = useState(1);
+	const [count, setCount] = useState(1);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	function handlePreviousStep () {
+		if (step > 1) setStep(curStep => curStep - 1);
+	}
+
+	function handleNextStep () {
+		setStep(curStep => curStep + 1);
+	}
+
+	function handlePreviousCount () {
+		setCount(curCount => curCount - step);
+	}
+
+	function handlePNextCount () {
+		setCount(curCount => curCount + step);
+	}
+
+	return <div className="date-component">
+		<div className="step">
+			<button onClick={handlePreviousStep}>-</button>
+			<span>Step: {step}</span>
+			<button onClick={handleNextStep}>+</button>
+		</div>
+		<div className="count">
+			<button onClick={handlePreviousCount}>-</button>
+			<span>Count: {count}</span>
+			<button onClick={handlePNextCount}>+</button>
+		</div>
+		<p className="phase">
+			{count === 0 && `Today is `}
+			{count > 0 && `${count} days from today is `}
+			{count < 0 && `${count} days ago was `}
+			{new Date(Date.now() + count * 24 * 60 * 60 * 1000).toString().substring(0, 15)}
+		</p>
+	</div>;
 }
 
-export default App
+export default App;
