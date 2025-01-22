c3953d82d4be2274ed8aa6c819c8e26e71a7084e
Freskies <43386854+Freskies@users.noreply.github.com>
2025-01-22 11:49:20 +0100

    step component

--- challenge-01/src/index.js
+++ challenge-01/src/index.js
@@ -3,52 +3,68 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 const valerio = {
-	image: "https://www.fibisromagna.it/foto/6098Foto_6992.jpg",
-	longName: "Giacchini Valerio",
-	description:
-		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto officia magni saepe! Dicta, exercitationem sit nobis praesentium quis vitae neque illo rem quaerat autem eligendi molestias omnis ab doloribus incidunt!",
-	skills: [
-		{ skillName: "HTML+CSS", color: "#00f" },
-		{ skillName: "Javascript", color: "#ff0" },
-		{ skillName: "Web Design", color: "#0f0" },
-		{ skillName: "Git and Github", color: "#f00" },
-		{ skillName: "React", color: "#0ff" },
-		{ skillName: "Python", color: "#f0f" },
-	],
+    image: "https://www.fibisromagna.it/foto/6098Foto_6992.jpg",
+    longName: "Giacchini Valerio",
+    description:
+        "I am a web developer with a passion for web design and user experience." +
+        "I am currently studying at the University of Bologna and" +
+        "I am looking for a job as a junior front-end developer.",
+    skills: [
+        {skillName: "HTML+CSS", color: "#00f", level: 3},
+        {skillName: "Javascript", color: "#ff0", level: 2},
+        {skillName: "Web Design", color: "#0f0", level: 1},
+        {skillName: "Git and Github", color: "#f00", level: 2},
+        {skillName: "React", color: "#0ff", level: 1},
+        {skillName: "Python", color: "#f0f", level: 3},
+    ],
 };
 
 function App() {
-	return <Card person={valerio} />;
+    return <Card person={valerio}/>;
 }
 
-function Card({ person: { image: url, longName, description, skills } }) {
-	return (
-		<article className="card">
-			<img src={url} alt={longName} />
-			<h3>{longName}</h3>
-			<p>{description}</p>
-			<Skills skills={skills} />
-		</article>
-	);
+function Card({person: {image: url, longName, description, skills}}) {
+    return (
+        <article className="card">
+            <img src={url} alt={longName}/>
+            <h3>{longName}</h3>
+            <p>{description}</p>
+            <Skills skills={skills}/>
+        </article>
+    );
 }
 
-function Skills({ skills }) {
-	return (
-		<div className="skills">
-			{skills.map(skill => (
-				<Skill skill={skill} key={skill.longName} />
-			))}
-		</div>
-	);
+function Skills({skills}) {
+    return (
+        <div className="skills">
+            {skills.map(skill => (
+                <Skill skill={skill} key={skill.longName}/>
+            ))}
+        </div>
+    );
 }
 
-function Skill({ skill: { skillName, color } }) {
-	return <span style={{ backgroundColor: color }}>{skillName}</span>;
+function Skill({skill: {skillName, color, level}}) {
+    return <span style={{backgroundColor: color}}>
+		{skillName} {
+        (() => {
+            switch (level) {
+                case 1:
+                    return "💧";
+                case 2:
+                    return "🔥";
+                case 3:
+                    return "⭐";
+                default:
+                    return null;
+            }
+        })()}
+	</span>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
 );
