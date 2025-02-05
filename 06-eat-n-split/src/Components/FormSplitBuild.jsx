10d3a2a177d7669566fcfc83683391a3552ffb4d
Freskies <43386854+Freskies@users.noreply.github.com>
2025-02-05 10:50:20 +0100

    switch user

--- 06-eat-n-split/src/Components/FormSplitBuild.jsx
+++ 06-eat-n-split/src/Components/FormSplitBuild.jsx
@@ -1,26 +1,57 @@
 import { friendType } from "../Friends.js";
+import { useState } from "react";
 
 FormSplitBuild.propTypes = {
-	friend: friendType
+	friend: friendType,
 };
 
-export default function FormSplitBuild ({ friend: {id, name, balance} }) {
+export default function FormSplitBuild ({ friend: { id, name, balance } }) {
+	const [bill, setBill] = useState("");
+	const [yourExpense, setYourExpense] = useState("");
+	const [friendExpense, setFriendExpense] = useState("");
+	const [whoPays, setWhoPays] = useState("user");
+
+	function getExpense (bill, expense) {
+		return bill >= expense ? expense : bill;
+	}
+
 	return <form className="form-split-bill">
 		<h2>Split a bill with {name}</h2>
 
 		<label htmlFor="10">💰 Bill value</label>
-		<input type="text" id="10"/>
+		<input
+			type="number"
+			id="10"
+			value={bill}
+			onChange={(e) => setBill(`${+e.target.value}`)}
+		/>
 
 		<label htmlFor="11">🧑‍🦰 Your expense</label>
-		<input type="text" id="11"/>
+		<input
+			type="number"
+			id="11"
+			value={whoPays === "user" ? yourExpense : bill - friendExpense}
+			onChange={(e) => setYourExpense(`${getExpense(+bill, +e.target.value)}`)}
+			disabled={whoPays === "friend"}
+		/>
 
-		<label htmlFor="12">👨‍👩‍👧‍👦 X's expense</label>
-		<input type="text" id="12" disabled/>
+		<label htmlFor="12">👨‍👩‍👧‍👦 {name}'s expense</label>
+		<input
+			type="number"
+			id="12"
+			value={whoPays === "friend" ? friendExpense : bill - yourExpense}
+			onChange={(e) => setFriendExpense(`${getExpense(+bill, +e.target.value)}`)}
+			disabled={whoPays === "user"}
+		/>
 
 		<label htmlFor="13">🤑 Who is paying the bill</label>
-		<select id="13">
+		<select
+			id="13"
+			value={whoPays}
+			onChange={(e) => setWhoPays(e.target.value)}
+		>
 			<option value="user">You</option>
-			<option value="friend">X</option>
+			<option value="friend">{name}</option>
 		</select>
 	</form>;
 }
