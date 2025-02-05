b39718b64e1d62aea05763f3d0f08c6ff698f0f0
Freskies <43386854+Freskies@users.noreply.github.com>
2025-02-05 09:50:42 +0100

    friend list

--- 06-eat-n-split/src/App.jsx
+++ 06-eat-n-split/src/App.jsx
@@ -2,12 +2,19 @@ import FriendsList from "./Components/FriendsList.jsx";
 import { initialFriends } from "./Friends.js";
 import FormAddFriend from "./Components/FormAddFriend.jsx";
 import FormSplitBuild from "./Components/FormSplitBuild.jsx";
+import { useState } from "react";
 
 export default function App () {
+	const [friendList, setFriendList] = useState(initialFriends);
+
+	function handleAddFriend (friend) {
+		setFriendList((friendList) => [...friendList, friend])
+	}
+
 	return <div className="app">
 		<div className="sidebar">
-			<FriendsList friends={initialFriends}/>
-			<FormAddFriend></FormAddFriend>
+			<FriendsList friends={friendList}/>
+			<FormAddFriend onAddFriend={handleAddFriend}></FormAddFriend>
 		</div>
 		<FormSplitBuild></FormSplitBuild>
 	</div>;
--- 06-eat-n-split/src/Components/FormAddFriend.jsx
+++ 06-eat-n-split/src/Components/FormAddFriend.jsx
@@ -1,14 +1,42 @@
 import Button from "./Button.jsx";
+import { useState } from "react";
 
-export default function FormAddFriend() {
+export default function FormAddFriend ({ onAddFriend }) {
+	const [displayForm, setDisplayForm] = useState(false);
+	const [name, setName] = useState("");
+	const [image, setImage] = useState("https://i.pravatar.cc/48");
 
-	return <form className="form-add-friend">
-		<label htmlFor="1">👪 Friend name</label>
-		<input type="text" id="1"/>
+	function handleToggleForm () {
+		setDisplayForm((displayForm) => !displayForm);
+	}
 
-		<label htmlFor="2">🖼️ Image URL</label>
-		<input type="text" id="2"/>
+	function handleSubmit (e) {
+		e.preventDefault();
+		if (!name || !image) return;
 
-		<Button>Add</Button>
-	</form>
+		const id = crypto.randomUUID();
+		const newFriend = { name, image: `${image}?${id}`, balance: 0, id };
+
+		onAddFriend(newFriend);
+		setDisplayForm(false);
+
+		setName("");
+		setImage("https://i.pravatar.cc/48");
+	}
+
+	return <>
+		{displayForm &&
+			<form className="form-add-friend" onSubmit={handleSubmit}>
+				<label htmlFor="1">👪 Friend name</label>
+				<input type="text" id="1" value={name} onChange={e => setName(e.target.value)}/>
+
+				<label htmlFor="2">🖼️ Image URL</label>
+				<input type="text" id="2" value={image} onChange={e => setImage(e.target.value)}/>
+
+				<Button>Add</Button>
+			</form>
+		}
+
+		<Button onClick={handleToggleForm}>{displayForm ? "Close" : "Add Friend"}</Button>
+	</>;
 }
