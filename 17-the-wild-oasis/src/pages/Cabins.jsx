e3592895327f02585af39f1403655a9f6cadc7a7
Freskies <freskies.bico.apo@gmail.com>
2026-01-22 11:06:32 +0100

    add Supabase integration for cabin data retrieval

--- 17-the-wild-oasis/src/pages/Cabins.jsx
+++ 17-the-wild-oasis/src/pages/Cabins.jsx
@@ -1,9 +1,16 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { useEffect } from "react";
+import { getCabins } from "../services/apiCabins.js";
 
 function Cabins () {
+	useEffect(() => {
+		getCabins().then(cabins => console.log(cabins))
+	}, []);
+
 	return <Row type="horizontal">
 		<Heading>All cabins</Heading>
+		<img src="https://ldllxejdkhqyioxemtfi.supabase.co/storage/v1/object/public/cabin.images/cabin-001.jpg" alt=""/>
 		<p>TEST</p>
 	</Row>;
 }
---
+++ 17-the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://ldllxejdkhqyioxemtfi.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxkbGx4ZWpka2hxeWlveGVtdG" +
+	"ZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkwNjk3NzYsImV4cCI6MjA4NDY0NTc3Nn0.OjeStVEk6APZYrvhI7LaxSlbGCzAPnX5hV-xJp-P_sk";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase
